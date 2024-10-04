e437c3fecf8c22d592b6b29fd14a9db15bf1f2ce
Remco Ros <remcoros@live.nl>
2024-10-04 12:14:48 +0000

    add 'reconnect' setting and patch noVNC to fix issue after browser tab has been idle.

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,23 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "2.0.0" );
+  .fromMapping({
+    "2.0.0.1": {
+      up: compat.migrations.updateConfig(
+        (config: any) => {
+          config["reconnect"] = false;
+          return config;
+        },
+        true,
+        { version: "2.0.0.1", type: "up" },
+      ),
+      down: compat.migrations.updateConfig(
+        (config: any) => {
+          delete config["reconnect"];
+          return config;
+        },
+        true,
+        { version: "2.0.0.1", type: "down" },
+      ),
+    },
+  }, "2.0.0.1");
