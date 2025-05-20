f6fdd2f1eb7260e76d6293238d2c4cdbbc008cd7
Remco Ros <remcoros@live.nl>
2025-05-20 20:12:30 +0000

    sparrow 2.2.0 / remove yq / updated github actions

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -2,7 +2,7 @@ import { VersionInfo, VersionGraph, IMPOSSIBLE } from '@start9labs/start-sdk'
 
 export const versions = VersionGraph.of(
   VersionInfo.of({
-    version: '2.1.3:0.1',
+    version: '2.2.0:0.1',
     releaseNotes: 'Revamped for StartOS 0.4.0',
     migrations: {
       up: async ({ effects }) => {},
