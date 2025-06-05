cc80bfa9eac1c2651a4bf1fcd8e13f3edf76b5f5
Remco Ros <remcoros@live.nl>
2025-06-05 15:46:10 +0000

    update to sdk beta.26 / update kasm base container

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -84,7 +84,7 @@ export const manifest = setupManifest({
     bitcoind: {
       description: 'Used to connect to your Bitcoin node.',
       optional: true,
-      s9pk: 'https://github.com/Start9Labs/bitcoind-startos/releases/download/v28.1.0.0-alpha.6/bitcoind.s9pk',
+      s9pk: 'https://github.com/Start9Labs/bitcoind-startos/releases/download/v28.1.0.3-alpha.0/bitcoind.s9pk',
     },
     electrs: {
       description: 'Used to connect to your Bitcoin node.',
