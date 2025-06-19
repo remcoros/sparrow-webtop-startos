13f9257f99fdc3518ae030c4c9736bc245bdc3ed
Remco Ros <remcoros@live.nl>
2025-06-19 16:23:24 +0000

    startos sdk beta.30 / sparrow 2.2.3

--- startos/install/versions/v2_2_3.ts
+++ startos/install/versions/v2_2_3.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v2_2_2 = VersionInfo.of({
-  version: '2.2.2:0.1',
+export const v2_2_3 = VersionInfo.of({
+  version: '2.2.3:0.1',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {},
