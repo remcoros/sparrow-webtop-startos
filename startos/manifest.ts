99a169cb1ba3c3098a1697814f63fc9ca24b0382
Remco Ros <remcoros@live.nl>
2025-07-02 16:18:45 +0000

    update startos sdk to beta.32

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -84,12 +84,12 @@ export const manifest = setupManifest({
     bitcoind: {
       description: 'Used to connect to your Bitcoin node.',
       optional: true,
-      s9pk: 'https://github.com/Start9Labs/bitcoind-startos/releases/download/v28.1.0.3-alpha.0/bitcoind.s9pk',
+      s9pk: 'https://github.com/Start9Labs/bitcoind-startos/releases/download/v28.1.0.3-alpha.5/bitcoind.s9pk',
     },
     electrs: {
       description: 'Used to connect to your Bitcoin node.',
       optional: true,
-      s9pk: 'https://github.com/Start9Labs/electrs-startos/releases/download/v0.10.9/electrsV2.s9pk',
+      s9pk: 'https://github.com/Start9Labs/electrs-startos/releases/download/v0.10.9-1-alpha.1/electrs.s9pk',
     },
   },
 })
