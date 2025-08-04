55beaaed0e1b01cf366b208be95066e278fb1fea
Remco Ros <remcoros@live.nl>
2025-08-04 16:07:58 +0000

    cleanup and move one 'sed' command to Dockerfile

--- startos/install/versions/v2_2_3.ts
+++ startos/install/versions/v2_2_3.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
 export const v2_2_3 = VersionInfo.of({
-  version: '2.2.3:0.1',
+  version: '2.2.3:0.2',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {},
